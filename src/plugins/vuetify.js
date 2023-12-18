79ff113a2082ef2631a509ba8af36ae5bfcd96e2
ristonhen <ristonhen@gmail.com>
2023-12-18 10:33:06 +0700

    Upgrade package.json

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,5 +1,6 @@
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
+import '@/styles/main.scss'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
